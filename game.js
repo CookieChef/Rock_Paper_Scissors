19c5aeadf6fa54b5c871492c743cb5bee43095b4
Sara Martinez <s_martinez_1990@outlook.com>
2020-01-17 17:11:33 -0500

    working on js logic

--- game.js
+++ game.js
@@ -1,7 +1,14 @@
 console.log("Connected");
 
 $(document).ready(function () {
-    playGame = () => { 
+    
+    playGame =() => {
 
+        var winsText = document.getElementById("wins");
+        var lostText = document.getElementById("lost");
+        var tiesText = document.getElementById("ties");
+        var userChoiceText = document.getElementById("userChoice");
+        var computerChoiceText = document.getElementById("computerChoice");
     }
+
 })
