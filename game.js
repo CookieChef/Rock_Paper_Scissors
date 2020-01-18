894c0f8875ba86aa3c9ffb5ee8eb5e700e14a472
Sara Martinez <s_martinez_1990@outlook.com>
2020-01-17 19:00:02 -0500

    adding functions to game logic

--- game.js
+++ game.js
@@ -14,32 +14,50 @@ var tiesText = document.getElementById("ties");
 var userChoiceText = document.getElementById("userChoice");
 var computerChoiceText = document.getElementById("computerChoice");
 
-document.onkeyup = function (event) {
+var rock = document.getElementById("r");
+var paper = document.getElementById("p");
+var scissors = document.getElementById("s");
+
+function main () {
+    rock.addEventListener("click", function () {
+        // game("r");
+        console.log("rock");
+    })
+    paper.addEventListener("click", function () {
+        // game("p");
+        console.log("paper");
+    })
+    scissors.addEventListener("click", function () {
+        // game("s");
+        console.log("scissors");
+    })
+}
+
+main();
     //determined which key was pressed
-    var userGuess = event.key;
+    //var userGuess = event.key;
 
     //randomly chooses a choice from the array
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
     //game logic
-    if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+    // if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
 
-        if ((userGuess === "r" && computerGuess === "s") ||
-            (userGuess === "s" && computerGuess === "p") ||
-            (userGuess === "p" && computerGuess === "r")) {
-            wins++;
-        } else if (userGuess === computerGuess) {
-            ties++;
-        } else {
-            lost++;
-        }
+    //     if ((userGuess === "r" && computerGuess === "s") ||
+    //         (userGuess === "s" && computerGuess === "p") ||
+    //         (userGuess === "p" && computerGuess === "r")) {
+    //         wins++;
+    //     } else if (userGuess === computerGuess) {
+    //         ties++;
+    //     } else {
+    //         lost++;
+    //     }
 
         //displays the outcome and the user and computer choices. 
-        userChoiceText.textContent = "You chose: " + userGuess;
-        computerChoiceText.textContent = "The computer chose: " + computerGuess;
-        winsText.textContent = "wins: " + wins;
-        lostText.textContent = "losses: " + lost;
-        tiesText.textContent = "ties: " + ties;
+        // userChoiceText.textContent = "You chose: " + userGuess;
+        // computerChoiceText.textContent = "The computer chose: " + computerGuess;
+        // winsText.textContent = "wins: " + wins;
+        // lostText.textContent = "losses: " + lost;
+        // tiesText.textContent = "ties: " + ties;
 
-    }
-}
+    //}
